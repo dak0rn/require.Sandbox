595e9502a40272f6d083fac0d8ec3e340520c421
dak0rn <daniel@suitsoft.eu>
2015-02-13 07:33:17 +0100

    Added a test that ensures that require.Sandbox can be required like a legacy module

--- test/test.sandbox.js
+++ test/test.sandbox.js
@@ -352,4 +352,13 @@ describe('Sandbox', function(){
         });
     });
 
+    it('should be requirable by require.js', function(done){
+
+        require(['../index'], function(sb){
+            expect( sb ).to.equal( require.Sandbox );
+            done();
+        });
+
+    });
+
 });
