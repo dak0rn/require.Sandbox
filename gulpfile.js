700eee12f6f3e20ab094907a5239c4ac4447dd7e
dak0rn <daniel@suitsoft.eu>
2015-02-13 13:03:27 +0100

    Uglify task included

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,13 @@ var uglify = require('gulp-uglify');
 var phantom = require('gulp-mocha-phantomjs');
 
 gulp.task('lint', function(){
-    gulp.src('./index.js')
+    gulp.src('index.js')
         .pipe( jshint() )
         .pipe( jshint.reporter('default') );
+});
+
+gulp.task('uglify', function(){
+    gulp.src('index.js')
+        .pipe( uglify() )
+        .pipe( gulp.dest('index.min.js') );
 });
