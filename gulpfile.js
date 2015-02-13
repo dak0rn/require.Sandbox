2f5d7a70b0b7b92307a47a9fb36df7e64ed21af1
dak0rn <daniel@suitsoft.eu>
2015-02-13 13:02:08 +0100

    Created the lint task in the build file

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,13 @@
  * Build file for require.Sandbox
  */
 var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var phantom = require('gulp-mocha-phantomjs');
+
+gulp.task('lint', function(){
+    gulp.src('./index.js')
+        .pipe( jshint() )
+        .pipe( jshint.reporter('default') );
+});
